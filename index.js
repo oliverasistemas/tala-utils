a7dc7c0f85b133db05db4fc7eed27405764c020e
adolfoolivera <olivera.adolfo@gmail.com>
2018-02-09 16:14:17 -0300

    Sum Collection

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const {sortBy: ord} = require('lodash');
 
 exports.sortBy = (collection, column, asc) => asc ?  ord(collection, column) : ord(collection, column).reverse();
 exports.formatNumber = number => number && number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+exports.sumCollection = (col, key) => col.reduce((acc, val) => acc + Number(val[key]), 0);
