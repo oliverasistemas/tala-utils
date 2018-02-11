869d2fc923c74b05c380b1ae4c58df6c2daab746
adolfoolivera <olivera.adolfo@gmail.com>
2018-02-11 18:43:03 -0300

    Transpile

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const {sortBy: ord} = require('lodash');
+"use strict";
 
-exports.sortBy = (collection, column, asc) => asc ?  ord(collection, column) : ord(collection, column).reverse();
-exports.formatNumber = number => number && number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-exports.sumCollection = (col, key) => col.reduce((acc, val) => acc + Number(val[key]), 0);
+var _require = require('lodash'),
+    ord = _require.sortBy;
+
+exports.sortBy = function (collection, column, asc) {
+    return asc ? ord(collection, column) : ord(collection, column).reverse();
+};
+exports.formatNumber = function (number) {
+    return number && number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+};
+exports.sumCollection = function (col, key) {
+    return col.reduce(function (acc, val) {
+        return acc + Number(val[key]);
+    }, 0);
+};
